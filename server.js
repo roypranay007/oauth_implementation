13768936e55c2091d92c05bcbd6e97ee2f1f5535
roypranay007 <54672882+roypranay007@users.noreply.github.com>
2019-08-30 15:23:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 // Public route
-app.get('', (req, res)=>{
+app.get('/public', (req, res)=>{
   let deals = [
     {
     id: 1234,
